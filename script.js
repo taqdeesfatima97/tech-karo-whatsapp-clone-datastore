eac133260592cd8c5e3dfde9188acf6f40e3ffa7
taqdeesfatima97 <50422652+taqdeesfatima97@users.noreply.github.com>
2019-07-11 17:10:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+let techKaro =
+{
+    name: "'tech karo",
+    shortDetail: "tech karo is a  programe of web development",
+    longDetail: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ex unde obcaecati quidem eius maxime labore, corporis quasi fugit. Architecto aliquid quam necessitatibus esse perferendis beatae, magni temporibus veritatis quasi.",
+    period: "8 months",
+    students: [
+        {
+            name: "khadija-tul-kubra",
+            education: "graduated",
+            gender: "female",
+            cnic: "42000-7856463-4",
+            profession: "teacher and web devevlopber"
+
+        },
+        {
+            name: "kiran",
+            age: "21",
+            cnic: "42101 - 345678 - 2",
+            height: "5.9",
+            profession: "web developer",
+        },
+        {
+            name: "kokab",
+            age: "23",
+            cnic: "42101-345678-22",
+            course: "Tech Karo",
+            height: "5.7",
+            profession: "teacher and student and web developer"
+        },
+    ]
+}
+
+
+
+// let goodstudent = {
+//     name: "kiran",
+//     age: "21",
+//cnic:42101-345678-22
+//course:Tech Karo
+//     height: "5.9",
+//     profession: "pagal banana"
+// };
+
+// let students = [
+//  {
+//     name: "kokab",
+//     age: "23",
+//cnic:42101-345678-22
+//course:Tech Karo
+//     height: "5.7",
+//     profession: "teacher and student and web developer"
+// },
+// {
+//     name: "khadija",
+//     age: "21",
+//cnic:42101-345678-22
+//course:Tech Karo
+//     height: "5.9",
+//     profession: "teacher and web developer"
+// }]   
+
+let whatsApp = [
+    {
+        name: "Qaiser friend",
+        date: "11-07-19",
+        tiime: "1:00 P.M",
+        message: "kahan ho tum?",
+    },
+    {
+        name: "Sana Cousin",
+        date: "11-07-19",
+        time: "12:45 P.M",
+        message: "how are you?",
+    },
+    {
+        name: "Tooba",
+        date: "11-07-19",
+        time: "09:00 P.M",
+        message: "i am on the way",
+    },
+    {
+        name: "khadija",
+        date: "11-07-19",
+        time: "1:00 A.M",
+        message: "data store wala kaam krlia tm nay ?",
+    },
+]
